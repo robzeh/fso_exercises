ee0ffbbe76bbe48e9c3f6bfe9dccd6ab848d38b0
Robie Gonzales <robie.gonzaless@gmail.com>
2020-05-05 14:06:49 -0300

    NoteReducer

--- part6/src/App.js
+++ part6/src/App.js
@@ -1,34 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import noteReducer from "./reducers/noteReducer";
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    case "ZERO":
-      return 0;
-    default:
-      // if none of the above matches, code comes here
-      return state;
-  }
-};
+const store = createStore(noteReducer);
 
-const store = createStore(counterReducer);
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
 
 const App = () => {
   return (
     <div>
-      <div>{store.getState()}</div>
-      <button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>
-        plus
-      </button>
-      <button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>
-        minus
-      </button>
-      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>zero</button>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
