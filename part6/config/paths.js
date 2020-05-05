1f2a7227c7ae31c665e250f9106c0d1147a72290
Robie Gonzales <robie.gonzaless@gmail.com>
2020-05-05 16:18:07 -0300

    Refactored redux logic and seperated components

--- part6/config/paths.js
+++ part6/config/paths.js
@@ -55,7 +55,7 @@ module.exports = {
   appBuild: resolveApp("build"),
   appPublic: resolveApp("public"),
   appHtml: resolveApp("public/index.html"),
-  appIndexJs: resolveModule(resolveApp, "src/App"),
+  appIndexJs: resolveModule(resolveApp, "src/Index"),
   appPackageJson: resolveApp("package.json"),
   appSrc: resolveApp("src"),
   appTsConfig: resolveApp("tsconfig.json"),
